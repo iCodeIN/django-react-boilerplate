4e136b2c7258fa19848354375f98d365bc600e48
e1r0nd <a.merezhanyi@gmail.com>
2020-12-12 00:32:58 +0100

    fix

--- server/app/settings/prod.py
+++ server/app/settings/prod.py
@@ -9,6 +9,12 @@ DATABASES["default"]["ENGINE"] = "django.db.backends.postgresql"
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 DATABASES["default"]["CONN_MAX_AGE"] = int(os.getenv("CONN_MAX_AGE", default=60))
 
+MIDDLEWARE += [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 import django_on_heroku
 
 django_on_heroku.settings(locals())
