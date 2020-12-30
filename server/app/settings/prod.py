252b2e28fa423fd10b571e87e027c1200244f569
e1r0nd <a.merezhanyi@gmail.com>
2020-12-30 13:01:35 +0100

    fix

--- server/app/settings/prod.py
+++ server/app/settings/prod.py
@@ -2,4 +2,7 @@ from .base import *  # NOSONAR
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+ALLOWED_HOSTS = []
+allowed_hosts = os.getenv("ALLOWED_HOSTS") or ""
+for host in allowed_hosts.split(","):
+    ALLOWED_HOSTS.append(host)
